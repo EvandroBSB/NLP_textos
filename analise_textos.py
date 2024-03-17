e57e5e3dc15a71f2ba27757dccd27d9f0917e7e9
EvandroBSB <104323381+EvandroBSB@users.noreply.github.com>
2024-03-17 22:55:44 +0000

    Simplificando o main, com mais explicação do codigo

--- analise_textos.py
+++ analise_textos.py
@@ -1,88 +1,120 @@
-import os
-os.system("pip install -r requirements.txt")
+# Importações necessárias
 
-import streamlit as st
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
-import requests
-import nltk
-from nltk.corpus import stopwords
-from collections import Counter
-import PyPDF2
-import docx2txt
-from io import BytesIO
+import os #os: Usado para executar comandos do sistema operacional.
+import streamlit as st # streamlit: Framework para criar aplicativos web rápidos.
+from wordcloud import WordCloud # WordCloud: Biblioteca para gerar nuvens de palavras.
+import matplotlib.pyplot as plt # matplotlib.pyplot: Biblioteca para criar visualizações gráficas.
+from bs4 import BeautifulSoup # BeautifulSoup: Biblioteca para fazer parsing de documentos HTML e XML.
+import requests # requests: Biblioteca para fazer requisições HTTP.
+import nltk # nltk: Natural Language Toolkit, biblioteca para trabalhar com dados de linguagem humana.
+from nltk.corpus import stopwords # stopwords, Counter: Utilidades do nltk e collections para processamento de texto.
+from collections import Counter #usando para contar objetos como caracteres em uma string ou itens em uma lista.
+import PyPDF2 # PyPDF2 - Biblioteca para extrair texto de arquivos PDF
+import docx2txt # docx2txt - Biblioteca para extrair texto de arquivos DOCX.
+from io import BytesIO # BytesIO: Utilitário para trabalhar com streams de bytes em memória.
 
+# Baixa dados necessários do NLTK, como stopwords e o tokenizador.
 nltk.download('stopwords')
-nltk.download('punkt')  # Adicionando esta linha para baixar o tokenizador de sentenças
+nltk.download('punkt')
 
-def extract_text_from_pdf(file):
-    buffer = BytesIO(file.read())
-    reader = PyPDF2.PdfReader(buffer)
-    text = ''
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
+# Funções para processamento de texto
 
-def extract_text_from_docx(file_path):
-    text = docx2txt.process(file_path)
-    return text
+# Extrai texto de arquivos PDF.
+def extrair_texto_de_pdf(arquivo):
+    buffer = BytesIO(arquivo.read())
+    leitor = PyPDF2.PdfReader(buffer)
+    texto = ''
+    for pagina in leitor.pages:
+        texto += pagina.extract_text() or ''  # Adiciona texto da página ao total, lidando com None
+    return texto
 
-def get_text_from_web(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    paragraphs = soup.find_all('p')
-    text = ' '.join([p.get_text() for p in paragraphs])
-    return text
+# Extrai texto de arquivos DOCX.
+def extrair_texto_de_docx(caminho_arquivo):
+    texto = docx2txt.process(caminho_arquivo)
+    return texto
 
-def remove_stopwords(text):
-    stop_words = set(stopwords.words('portuguese'))
-    words = nltk.word_tokenize(text.lower())
-    filtered_words = [word for word in words if word.isalnum() and word not in stop_words]
-    return filtered_words
+# Obtém texto de uma página da web usando sua URL.
+def obter_texto_da_web(url):
+    resposta = requests.get(url)
+    sopa = BeautifulSoup(resposta.content, 'html.parser')
+    paragrafos = sopa.find_all('p')
+    texto = ' '.join(p.get_text() for p in paragrafos)
+    return texto
 
-def generate_wordcloud(text):
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+# Remove stopwords de um texto em português.
+def remover_stopwords(texto):
+    palavras_parada = set(stopwords.words('portuguese'))
+    palavras = nltk.word_tokenize(texto.lower())
+    palavras_filtradas = [palavra for palavra in palavras if palavra.isalnum() and palavra not in palavras_parada]
+    return ' '.join(palavras_filtradas)
+
+# Gera e exibe uma nuvem de palavras a partir de um texto.
+def gerar_nuvem_de_palavras(texto):
+    nuvem_palavras = WordCloud(width=800, height=400, background_color='white').generate(texto)
     plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.imshow(nuvem_palavras, interpolation='bilinear')
     plt.axis('off')
     st.pyplot(plt)
 
-def main():
-    text = ''  # Definindo a variável text como vazio
-    st.title('Análise Estatística de Texto')
-
-    input_option = st.radio('Selecione o tipo de entrada de dados:', ('PDF', 'Word', 'Link da Página', 'Texto Direto'))
-
-    if input_option == 'PDF':
-        file = st.file_uploader('Carregar PDF:', type=['pdf'])
-        if file is not None:
-            text = extract_text_from_pdf(file)
-    elif input_option == 'Word':
-        file = st.file_uploader('Carregar documento Word:', type=['docx'])
-        if file is not None:
-            text = extract_text_from_docx(file)
-    elif input_option == 'Link da Página':
-        url = st.text_input('Insira o URL da página:')
-        if url:
-            text = get_text_from_web(url)
-    else:
-        text = st.text_area('Insira o texto aqui:')
-
-    if text:
-        st.subheader('Texto Analisado:')
-        st.write(text)
-
-        filtered_words = remove_stopwords(text)
-        word_freq = Counter(filtered_words)
-        most_common_words = word_freq.most_common(20)
-
-        st.subheader('Top 20 Palavras Mais Frequentes (exceto stopwords):')
-        for word, freq in most_common_words:
-            st.write(f'{word}: {freq}')
-
-        st.subheader('Nuvem de Palavras:')
-        generate_wordcloud(' '.join(map(str, filtered_words)))
+# Início do aplicativo Streamlit
 
 if __name__ == "__main__":
-    main()
+    texto = ''  # Variável para armazenar o texto a ser analisado
+    st.title('Aplicação Web - Análise Estatística básica de Texto')  # Título do aplicativo
+
+    # Opções de entrada de dados pelo usuário
+    opcao_entrada = st.radio(
+        'Senhor (a) usuário (a), escolha o tipo de entrada de dados:',
+        ('Word', 'PDF', 'Link da Página', 'Texto Direto')
+    )
+
+    # Processa a entrada conforme a opção escolhida
+    
+    if opcao_entrada == 'Word':
+        arquivo = st.file_uploader('Carregar documento Word:', type=['docx'])
+        if arquivo is not None:
+            texto = extrair_texto_de_docx(arquivo)
+    elif opcao_entrada == 'PDF':
+        arquivo = st.file_uploader('Carregar PDF:', type=['pdf'])
+        if arquivo is not None:
+            texto = extrair_texto_de_pdf(arquivo)
+    elif opcao_entrada == 'Link da Página':
+        url = st.text_input('Insira o URL da página:')
+        if url:
+            texto = obter_texto_da_web(url)
+    else:
+        texto = st.text_area('Insira o texto aqui:')
+
+   # Se há texto, processa e exibe análises
+if texto:
+    # Adiciona um subtítulo no aplicativo Streamlit com o texto 'Texto Analisado:'
+    st.subheader('Texto extraido da base informada pelo usuário:')
+    
+    # Exibe o texto obtido na interface do aplicativo. Isso permite ao usuário visualizar o texto que está sendo analisado.
+    st.write(texto)  
+    
+    # Chama a função remover_stopwords para filtrar as stopwords do texto. Esta função retorna o texto sem as palavras comuns que não agregam significado significativo para análise de frequência.
+    texto_filtrado = remover_stopwords(texto)
+    
+    # Utiliza a classe Counter para contar a frequência de cada palavra no texto filtrado.
+    # O método split() divide o texto em uma lista de palavras, baseando-se em espaços, que é então passada para Counter.
+    frequencia_palavras = Counter(texto_filtrado.split())
+    
+    # Obtém as 20 palavras mais comuns e suas contagens usando o método most_common(20).
+    # Isso retorna uma lista de tuplas, onde cada tupla contém uma palavra e sua contagem.
+    palavras_mais_comuns = frequencia_palavras.most_common(20)
+    
+    # Adiciona outro subtítulo no aplicativo Streamlit para indicar que as próximas informações são as 20 palavras mais frequentes, excluindo stopwords.
+    st.subheader('Top 20 Palavras Mais Frequentes (exceto stopwords):')
+    
+    # Para cada par palavra-frequência nas 20 palavras mais comuns, exibe essa informação no aplicativo.
+    # O loop for itera sobre cada tupla na lista palavras_mais_comuns, extraindo a palavra e sua frequência e exibindo-as.
+    for palavra, frequencia in palavras_mais_comuns:
+        st.write(f'{palavra}: {frequencia}')
+    
+    # Adiciona um subtítulo para a nuvem de palavras, indicando que a próxima seção do aplicativo mostrará a nuvem de palavras.
+    st.subheader('Nuvem de Palavras:')
+    
+    # Gera a nuvem de palavras a partir do texto filtrado. A função gerar_nuvem_de_palavras cria uma visualização gráfica que mostra as palavras mais frequentes no texto, com o tamanho de cada palavra proporcional à sua frequência.
+    # Esta visualização é então exibida no aplicativo usando st.pyplot().
+    gerar_nuvem_de_palavras(texto_filtrado)
